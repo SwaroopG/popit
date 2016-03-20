051645b422fe4f84729f6e97b719992b14b26f10
Swaroop <swarooprajg@gmail.com>
2016-03-19 21:33:42 -0500

    Jar to war.
    Packaging changed from jar to war.
    Updated the login html.

--- src/main/java/com/poorjar/popit/PopitApplication.java
+++ src/main/java/com/poorjar/popit/PopitApplication.java
@@ -3,7 +3,9 @@ package com.poorjar.popit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * Entry point class of the Popit service.
@@ -12,8 +14,15 @@ import org.springframework.boot.context.web.SpringBootServletInitializer;
  */
 @SpringBootApplication
 @EnableAutoConfiguration
+@ComponentScan
 public class PopitApplication extends SpringBootServletInitializer {
-    public static void main(String[] args) {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(PopitApplication.class);
+    }
+
+    public static void main(String[] args) throws Exception {
         SpringApplication.run(PopitApplication.class, args);
     }
+
 }
