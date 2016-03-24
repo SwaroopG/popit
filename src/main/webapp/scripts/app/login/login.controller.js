9e119cd025c53af45417c330666b4fbb77e57bfe
amireddy <namireddy@dir.eeft.com>
2016-03-23 20:41:44 -0500

    use satellizer and UI changes

--- src/main/webapp/scripts/app/login/login.controller.js
+++ src/main/webapp/scripts/app/login/login.controller.js
@@ -2,27 +2,33 @@
 
 var module = angular.module('LoginController',[]);
 
-module.controller('LoginController', ['$scope', 'GAuth', 'GData', '$state', '$cookies', function clientList($scope, GAuth, GData, $state, $cookies) {
-    	if(GData.isLogin()){
-    		$state.go('home');
-    	}
-
-        var ifLogin = function() {
-            $cookies.put('userId', GData.getUserId());
-            $state.go('home');
-        };
-
-        $scope.doLogin = function() {
-            GAuth.checkAuth().then(
-                function () {
-                    ifLogin();
-                },
-                function() {
-                    GAuth.login().then(function(){
-                        ifLogin();
-                    });
-                }
-            );
-        };
-    }
-]);
+module.controller('LoginController', function($scope, $location, $auth, toastr) {
+    $scope.login = function() {
+      $auth.login($scope.user)
+        .then(function() {
+          toastr.success('You have successfully signed in!');
+          $location.path('/home');
+        })
+        .catch(function(error) {
+          toastr.error(error.data.message, error.status);
+        });
+    };
+    $scope.authenticate = function(provider) {
+      $auth.authenticate(provider)
+        .then(function() {
+          toastr.success('You have successfully signed in with ' + provider + '!');
+          $location.path('/home');
+        })
+        .catch(function(error) {
+          if (error.error) {
+            // Popup error - invalid redirect_uri, pressed cancel button, etc.
+            toastr.error(error.error);
+          } else if (error.data) {
+            // HTTP response error from server
+            toastr.error(error.data.message, error.status);
+          } else {
+            toastr.error(error);
+          }
+        });
+    };
+  });
